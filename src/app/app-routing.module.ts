ffac34345b396687c1008697fa68714e4b060c34
Nicolas Paxão da Silva <89174235+nicolaspaxao@users.noreply.github.com>
2023-08-28 09:56:24 -0300

    fixing error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '', loadChildren: () => import('../app/modules/modules-routing.module').then(m => m.ModulesRoutingModule) }
 ];
 
